5d7ef6092e2d9ca06ac803413640a7951f4752f3
Revanth <97815884+ThiruvidhiRevanth@users.noreply.github.com>
2022-01-16 16:41:45 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+  int a,b,c, average;
+       printf("enter the value of a:");
+       scanf("%d",&a);
+      printf("enter the value of b:");
+     scanf("%d",&b);
+    c=a+b;
+    average=c/2;
+    printf("average:%d", average);
+    return 0;
+} 
